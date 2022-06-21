573c295abaf9e503bc085cd8bfe1c324d65be88c
Rui Emanuel Vieira <64679344+E-RuiL-Vieira@users.noreply.github.com>
2022-06-21 02:09:31 +0000

    Melhorei o enum

--- Navio.java
+++ Navio.java
@@ -6,15 +6,26 @@ public class Navio{
 	private int comprimento;
 	private int posicaoX;
 	private int posicaoY;
-        private TipoNavio tipo; 
+  private TipoNavio tipo; 
         
-        enum TipoNavio{}
+	enum TipoNavio{
+    NAVIOTANQUE(4), 
+		PORTAAVIOES(5),
+		SUBMARINO(3),
+		CONTRATORPEDOS(2);
+		
+		private final int c;
+		TipoNavio(int c){ this.c = c;      }
+    private int comprimento() {return c; }
+    
+	}
 
-	public Navio(int x, int y){
+	public Navio(int x, int y, TipoNavio tipo){
 		navioAfundado = false;
 		this.posicaoX = x;
 		this.posicaoY = y;
-		
+  	this.comprimento = tipo.comprimento();
+                    
 	}
 	
 }
