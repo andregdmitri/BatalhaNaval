c78a5d69bbf6d150aa6a1a94cd1c3957cc454f1f
André Guarnier De Mitri <51839703+andregdmitri@users.noreply.github.com>
2022-06-22 22:51:00 +0000

    Gui adicionado

---
+++ GUI.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.batalhanaval;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class GUI implements ActionListener {
+    
+    JLabel label;
+    JFrame frame;
+    JPanel panel;
+    int i = 0;
+    STATE state;
+    
+    private enum STATE {
+        MENU,
+        GAME
+    }
+
+    public GUI() {
+        frame = new JFrame();
+        
+        JButton button_single = new JButton("Singleplayer");
+        JButton button_multi = new JButton("Multiplayer");
+        state = STATE.MENU;
+        button_single.addActionListener(this);
+        button_multi.addActionListener(this);
+        
+        label = new JLabel("Escolha o modo de jogo");
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        //panel.setLayout(new GridLayout());
+        panel.add(button_single);
+        panel.add(button_multi);
+        
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Batalha Naval");
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getSource());
+    }
+}
